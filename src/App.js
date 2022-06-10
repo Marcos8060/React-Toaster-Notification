1e8246f5366881f924bce518f115fdd90d8ab03f
Marcos8060 <marcosgav80@gmail.com>
2022-06-10 15:38:47 +0300

    created a suucess toaster

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
+
+  const notify = ()=> toast.success('Wow so easy!!')
+
   return (
     <div className="App">
       <h3>React Toaster Notifications</h3>
-      
+      <button onClick={notify}>Click Me!</button>
+      <ToastContainer />
     </div>
   );
 }
